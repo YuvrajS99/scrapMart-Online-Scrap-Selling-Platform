eeafc1c85063ba28642ad153950c72cd04fad402
Yuvraj Sanap <110754561+YuvrajS99@users.noreply.github.com>
2026-03-29 19:11:13 +0530

    Use environment variable for server port

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", require("./routes/authRoutes"));
 
-const PORT = 5000;
+const PORT =process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
